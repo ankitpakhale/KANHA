0209c9674bda0d1bc332e17bb3ba6e3e7ad9d7d1
Ankit Pakhale <akp3067@gmail.com>
2024-12-09 16:47:28 +0530

    modified start application bash file

--- start_application.sh
+++ start_application.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# Navigate to the src directory
+
+# remove all __pycache__ directories excluding those under .venv
+find . -type d -name "__pycache__" -not -path "./.venv/*" -exec rm -r {} \;
+
+# navigate to the src directory
 cd src || { echo "src directory not found!"; exit 1; }
-# Execute the main.py file
+
+# execute the main.py file
 python3 main.py
